a23effd33c3544cf58ad05fcf42985081fea32e1
Jason Kai <tkai@uwo.ca>
2017-08-17 10:34:16 -0400

    script organization

---
+++ install/neurodebian.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Sets up NeuroDebian key to install software packages
+
+wget -O- http://neuro.debian.net/lists/xenial.us-nh.full | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
+
+sudo apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9 
+
+sudo apt-get update
